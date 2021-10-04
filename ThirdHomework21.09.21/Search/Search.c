3584761a1430e6395bd26c0312a15fd019a12362
Niksen111 <nikiksen2003@gmail.com>
2021-10-04 10:55:17 +0300

    Added tests of sorting functions

--- ThirdHomework21.09.21/Search/Search.c
+++ ThirdHomework21.09.21/Search/Search.c
@@ -120,6 +120,35 @@ bool binarySearch(const int theArray[], const int lengthOfTheArray, const int th
     return false;
 }
 
+bool checkThatArrayIsSorted(const int theArray[], const int lengthOfTheArray)
+{
+    for (int i = 0; i < lengthOfTheArray - 1; ++i)
+    {
+        if (theArray[i] > theArray[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool checkThatAnArraysAreIdentical(const int firstArray[],
+    const int secondArray[], const int lengthOfArrays)
+{
+    bool identical = true;
+
+    for (int i = 0; i < lengthOfArrays; ++i)
+    {
+        if (firstArray[i] != secondArray[i])
+        {
+            identical = false;
+            break;
+        }
+    }
+
+    return identical;
+}
+
 bool testBinarySearch()
 {
     int array1[10] = { 0, 1, 1, 2, 3, 3, 4, 5, 6, 7 };
@@ -130,9 +159,73 @@ bool testBinarySearch()
         binarySearch(array2, 15, 13) && !binarySearch(array2, 15, -9));
 }
 
+bool testInsertionSort()
+{
+    int firstArray[5] = { 2, 3, 1, 5, 2 };
+    const int sortedTheFirstArray[5] = { 1, 2, 2, 3, 5 };
+    insertionSort(firstArray, 5);
+
+    int secondArray[3] = { -1, -1, -1 };
+    const int sortedTheSecondtArray[3] = { -1, -1, -1 };
+    insertionSort(secondArray, 3);
+
+    int thirdArray[10] = { 2, 5, 5, 1, 8, 3, 9, 4, 0, 7 };
+    const int sortedTheThirdArray[10] = { 0, 1, 2, 3, 4, 5, 5, 7, 8, 9 };
+    insertionSort(thirdArray, 10);
+
+    return (checkThatAnArraysAreIdentical(firstArray, sortedTheFirstArray, 5) &&
+        checkThatAnArraysAreIdentical(secondArray, sortedTheSecondtArray, 3) &&
+        checkThatAnArraysAreIdentical(thirdArray, sortedTheThirdArray, 10));
+}
+
+bool testMyQSortOnRandomArrays(const int maxLengthOfTheArrays, const int numberOfArrays)
+{
+    srand((unsigned)time(NULL));
+
+    int* randomArray = (int*)calloc(maxLengthOfTheArrays, sizeof(int));
+    bool sortIsWorking = true;
+
+    if (randomArray == NULL)
+    {
+        return true;
+    }
+
+    for (int i = 0; i < numberOfArrays; ++i)
+    {
+        int lengthOfTheArray = rand() % maxLengthOfTheArrays;
+        arrayWithRandomElements(randomArray, lengthOfTheArray, 1000);
+        int supportElement = randomArray[0];
+        myQSort(randomArray, lengthOfTheArray);
+
+        if (!checkThatArrayIsSorted(randomArray, lengthOfTheArray))
+        {
+            printf("Wrong myQSort on the array:\n");
+            printArray(randomArray, lengthOfTheArray);
+            sortIsWorking = false;
+            break;
+        }
+    }
+    free(randomArray);
+    return sortIsWorking;
+}
+
+bool testMyQSort()
+{
+    int array1[5] = { 2, 5, 1, 6, -2 };
+    int array2[10] = { 3, 1, 1, 7, -2, 6, -2, -6, 20, 10 };
+    int array3[20] = { 5, 2, 5, 2, 6, 2, 6, 0, -3, 12,
+        22, -22, 3, 1, 5, -6, 0, 222, 1, -50 };
+    myQSort(array1, 5);
+    myQSort(array2, 10);
+    myQSort(array3, 20);
+
+    return (checkThatArrayIsSorted(array1, 5) && checkThatArrayIsSorted(array2, 10) &&
+        checkThatArrayIsSorted(array3, 20));
+}
+
 int main()
 {
-    if (!testBinarySearch())
+    if (!testBinarySearch() || !testInsertionSort() || !testMyQSort || !testMyQSortOnRandomArrays(1000, 1000))
     {
         printf("Tests failed(\n");
         return 0;
