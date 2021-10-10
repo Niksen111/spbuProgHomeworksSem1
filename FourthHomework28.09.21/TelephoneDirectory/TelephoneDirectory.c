af628fc2a4454600ee075da9da4ec9a9391c7818
Niksen111 <nikiksen2003@gmail.com>
2021-10-11 00:49:32 +0300

    Reading function fixed, added add function

--- FourthHomework28.09.21/TelephoneDirectory/TelephoneDirectory.c
+++ FourthHomework28.09.21/TelephoneDirectory/TelephoneDirectory.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <locale.h>
+#include <string.h>
 
 typedef struct
 {
@@ -8,7 +9,15 @@ typedef struct
     char name[30];
 }TelephoneDerectory;
 
-int readBaseOfNumbers(char fileName[], TelephoneDerectory userDataBase[])
+void printCharArray(const char theArray[], const int lengthOfTheArray)
+{
+    for (int i = 0; i < lengthOfTheArray; ++i)
+    {
+        printf("%c", theArray[i]);
+    }
+}
+
+int readTelephoneDerectoryFromFile(const char fileName[], TelephoneDerectory userDataBase[], int *lengthOfBase)
 {
     FILE* file = fopen(fileName, "r");
     if (file == NULL)
@@ -16,14 +25,14 @@ int readBaseOfNumbers(char fileName[], TelephoneDerectory userDataBase[])
         return -1;
     }
     char symbol = '\0';
-    int currentNumber = 0;
+    *lengthOfBase = 1;
     while (true)
     {
         for (int i = 0; i < 20; ++i)
         {
             if ((symbol = fgetc(file)) != EOF)
             {
-                *userDataBase[currentNumber].phone = symbol;
+                userDataBase[*lengthOfBase - 1].phone[i] = symbol;
             }
             else
             {
@@ -40,7 +49,7 @@ int readBaseOfNumbers(char fileName[], TelephoneDerectory userDataBase[])
         {
             if ((symbol = fgetc(file)) != EOF)
             {
-                *userDataBase[currentNumber].name = symbol;
+                userDataBase[*lengthOfBase - 1].name[i] = symbol;
             }
             else
             {
@@ -48,12 +57,47 @@ int readBaseOfNumbers(char fileName[], TelephoneDerectory userDataBase[])
                 return 0;
             }
         }
+        if ((symbol = fgetc(file)) == EOF)
+        {
+            fclose(file);
+            return 0;
+        }
+        ++*lengthOfBase;
     }
 }
 
-void userInteraction(char fileName[])
+void fillArrayWithSpaces(char theArray[], const int lengthOfTheArray)
 {
-    
+    for (int i = 0; i < lengthOfTheArray; ++i)
+    {
+        theArray[i] = ' ';
+    }
+}
+
+void printBase(TelephoneDerectory userDataBase[], int lengthOfBase)
+{
+    for (int i = 0; i < lengthOfBase; ++i)
+    {
+        printCharArray(userDataBase[i].phone, 20);
+        printf(" ");
+        printCharArray(userDataBase[i].name, 30);
+        printf("\n");
+    }
+}
+
+int addEntry(TelephoneDerectory userDataBase[], int *lengthOfBase, char phone[], char name[])
+{
+    strcpy(userDataBase[*lengthOfBase].phone, phone);
+    strcpy(userDataBase[*lengthOfBase].name, name);
+    ++*lengthOfBase;
+}
+
+void userInteraction(const char fileName[])
+{
+    TelephoneDerectory baseOfNumbers[100] = { 0 };
+    int lengthOfBase = 0;
+
+    readTelephoneDerectoryFromFile(fileName, baseOfNumbers, &lengthOfBase);
     int numberOfOperation = -1;
     while (true)
     {
@@ -65,23 +109,31 @@ void userInteraction(char fileName[])
         }
         else if (numberOfOperation == 1)
         {
-            return;
+            fillArrayWithSpaces(baseOfNumbers[lengthOfBase].phone, 20);
+            fillArrayWithSpaces(baseOfNumbers[lengthOfBase].name, 30);
+            char phone[20] = { 0 };
+            char name[30] = { 0 };
+            printf("¬ведите номер телефона (не более 20 символов):\n");
+            scanf_s(" %[^\n]%*s", phone, 20);
+            printf("¬веидите им€ контакта (не более 30 символов):\n");
+            scanf_s(" %[^\n]%*s", name, 30);
+            addEntry(baseOfNumbers, &lengthOfBase, phone, name);
         }
         else if (numberOfOperation == 2)
         {
-            return;
+            printBase(baseOfNumbers, lengthOfBase);
         }
         else if (numberOfOperation == 3)
         {
-            return;
+            
         }
         else if (numberOfOperation == 4)
         {
-            return;
+            
         }
         else if (numberOfOperation == 5)
         {
-            return;
+            
         }
         else if (numberOfOperation == 6)
         {
@@ -97,9 +149,9 @@ void userInteraction(char fileName[])
 int main()
 {
     setlocale(LC_ALL, "Rus");
-    char fileName[40] = { 0 };
+    /*char fileName[40] = { 0 };
     printf("¬ведите им€ файла(телефонного справочника) формата .txt\n");
-    scanf("%s", fileName);
+    scanf("%s", fileName);*/
     printf("0 - выйти\n1 - добавить запись(им€ и телефон)\n2 - распечатать все имеющиес€ записи\n3 - найти телефон по имени\n4 - найти им€ по телефону\n5 - сохранить текущие данные в файл\n6 - покзать список операций\n");
-    userInteraction(fileName);
+    userInteraction("directory.txt");
 }
