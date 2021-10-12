af2863d0bcf2c1e77f8928976344dc8bad7caf9f
Niksen111 <nikiksen2003@gmail.com>
2021-10-12 14:19:43 +0300

    Added header

---
+++ FourthHomework28.09.21/ModulesAndFiles/myQSort.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void myQSort(int theArray[], const int lengthOfTheArray);
+
+void insertionSort(int theArray[], int lengthOfTheArray);
+
+void swap(int* firstVariable, int* secondVariable);
