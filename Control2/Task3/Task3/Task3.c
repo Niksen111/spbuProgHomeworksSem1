c533f0cb34a9402e0d6784fb32c6b7f0a09b40f8
Niksen111 <nikiksen2003@gmil.com>
2021-10-19 14:51:06 +0300

    Added readFromFile and lengthOfFile functions

--- Control2/Task3/Task3/Task3.c
+++ Control2/Task3/Task3/Task3.c
@@ -1,7 +1,69 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void printArray(const int theArray[], const int lengthOfTheArray)
+{
+    for (int i = 0; i < lengthOfTheArray; ++i)
+    {
+        printf("%d ", theArray[i]);
+    }
+    printf("\n\n");
+}
+
+int lengthArrayFromFile(const char nameOfFile[], int* lengthOfArray)
+{
+    FILE* file = fopen(nameOfFile, "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    *lengthOfArray = 0;
+    int temporary = 0;
+    int readBytes = fscanf(file, "%d", &temporary);
+    while (readBytes >= 0)
+    {
+        ++* lengthOfArray;
+        readBytes = fscanf(file, "%d", &temporary);
+    }
+    fclose(file);
+    return 0;
+}
+
+int readArrayFromFile(const char nameOfFile[], int theArray[], int lengthOfArray)
+{
+    FILE* file = fopen(nameOfFile, "r");
+    if (file == NULL)
+    {
+        return -1;
+    }
+    for (int i = 0; i < lengthOfArray; ++i)
+    {
+        fscanf(file, "%d", &theArray[i]);
+    }
+    fclose(file);
+    return 0;
+}
 
 int main()
 {
+    int lengthOfG = 0;
+    if (lengthArrayFromFile("g.txt", &lengthOfG) == -1)
+    {
+        printf("File g.txt not found!");
+        return -1;
+    }
+    if (lengthOfG != 1)
+    {
+        printf("File g.txt is incorrect!");
+        return -1;
+    }
+    int numFromGFile = 0;
+    if (readArrayFromFile("g.txt", &numFromGFile, 1) == -1)
+    {
+        printf("File g.txt not found!");
+        return -1;
+    }
     
 }
 
