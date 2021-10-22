d706f67a6ced609ddf51aa8529e37cf28d778a5b
Niksen111 <nikiksen2003@gmail.com>
2021-10-22 23:50:02 +0300

    Created Stack project, added structure Stack and functions for the Stack, added test (By mistake, everything was written in master)

---
+++ FifthHomework12.10.21/Stack/stack.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <stdbool.h>
+
+// type of data that will be stored in the stack
+typedef int Value;
+
+// stack structure
+typedef struct Stack
+{
+    Value value;
+    struct Stack* next;
+} Stack;
+
+// created an empty stack
+Stack* createStack();
+
+// checks if the stack is empty
+bool stackIsEmpty(Stack** head);
+
+// adds an element to the head of the stack
+int push(Stack** head, Value value);
+
+// removes an element form the head of the stack and returns it
+Value pop(Stack** head, int* errorCode);
+
+// removes the entire stack
+int deleteStack(Stack** head);
+
+// prints the entire stack
+void printStack(Stack** head);
