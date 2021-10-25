7d7d585d0147334626a40c95e69b070fd2696b89
Niksen111 <nikiksen2003@gmail.com>
2021-10-25 14:11:32 +0300

    Code transferred from .h to .c

--- FifthHomework12.10.21/SortingStationAlgorithm/sortingStationAlgorithm.c
+++ FifthHomework12.10.21/SortingStationAlgorithm/sortingStationAlgorithm.c
@@ -1,8 +1,39 @@
 #include "sortingStationAlgorithm.h"
-
+#include <stdlib.h>
+#include "stack.h"
+#include "advancedBracketBalance.h"
 
 char* convertToPostfixForm(char infix[], int* errorCode)
 {
-
+    if (!areBracketsBalanced(infix, errorCode))
+    {
+        return NULL;
+    }
+    int lengthOfInfix = 0;
+    for (int i = 0; infix[i] != '\0'; ++i)
+    {
+        ++lengthOfInfix;
+    }
+    char operations[4] = { '+', '-', '*', '/' };
+    Stack* stack = createStack();
+    char* postfix = calloc(lengthOfInfix, sizeof(char));
+    if (postfix == NULL)
+    {
+        *errorCode = -11;
+        return NULL;
+    }
+    int currentIndex = 0;
+    for (int i = 0; i < lengthOfInfix; ++i)
+    {
+        if ((int) infix[i] >= (int) '0' && (int) infix[i] <= (int) '9' ||
+            infix[i] == ' ')
+        {
+            postfix[currentIndex] = infix[i];
+            ++currentIndex;
+        }
+        else
+        {
+            return NULL;
+        }
+    }
 }
-
--- FifthHomework12.10.21/SortingStationAlgorithm/sortingStationAlgorithm.h
+++ FifthHomework12.10.21/SortingStationAlgorithm/sortingStationAlgorithm.h
@@ -1,41 +1,4 @@
 #pragma once
-#include <stdlib.h>
-#include "stack.h"
-#include "advancedBracketBalance.h"
 
 // Converts an expression from the infix form to the postfix form
-char* convertToPostfixForm(char infix[], int* errorCode)
-{
-    if (!areBracketsBalanced(infix, errorCode))
-    {
-        *errorCode = -10;
-        return NULL;
-    }
-    int lengthOfInfix = 0;
-    for (int i = 0; infix[i] != '\0'; ++i)
-    {
-        ++lengthOfInfix;
-    }
-
-    Stack* stack = createStack();
-    char* postfix = calloc(lengthOfInfix, sizeof(char));
-    if (postfix == NULL)
-    {
-        *errorCode = -11;
-        return NULL;
-    }
-    int currentIndex = 0;
-    for (int i = 0; i < lengthOfInfix; ++i)
-    {
-        if ((int) infix[i] >= (int) '0' && (int) infix[i] <= (int) '9' ||
-            infix[i] == ' ')
-        {
-            postfix[currentIndex] = infix[i];
-            ++currentIndex;
-        }
-        else
-        {
-            return NULL;
-        }
-    }
-}
+char* convertToPostfixForm(char infix[], int* errorCode);
