d85efb87270ba2f80d38a81c3fbc7ebdd709e746
Niksen111 <nikiksen2003@gmail.com>
2021-10-25 21:25:04 +0300

    The conversion algorithm is written, hand-tested on a single expression

--- FifthHomework12.10.21/SortingStationAlgorithm/main.c
+++ FifthHomework12.10.21/SortingStationAlgorithm/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "sortingStationAlgorithm.h"
+#include "postfixCalculator.h"
 
 int main() {
-    printf("Hello, World!\n");
+    char expression[100] = "1 + 2 * 3 + 4 * (5 + 6 * 7)";
+    int errorCode1 = 0;
+    int errorCode2 = 0;
+    char* postfix = convertToPostfixForm(expression, &errorCode1);
+    printf("%d\n", errorCode1);
+    printf("%s\n", postfix);
+    printf("%d %d", calculatePostfixExpression(postfix, &errorCode2), errorCode2);
+
     return 0;
 }
--- FifthHomework12.10.21/SortingStationAlgorithm/sortingStationAlgorithm.c
+++ FifthHomework12.10.21/SortingStationAlgorithm/sortingStationAlgorithm.c
@@ -3,6 +3,18 @@
 #include "stack.h"
 #include "advancedBracketBalance.h"
 
+int findElement(const char theArray[], const char element)
+{
+    for (int i = 0; theArray[i] != '\0'; ++i)
+    {
+        if (theArray[i] == element)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 char* convertToPostfixForm(char infix[], int* errorCode)
 {
     if (!areBracketsBalanced(infix, errorCode))
@@ -14,7 +26,7 @@ char* convertToPostfixForm(char infix[], int* errorCode)
     {
         ++lengthOfInfix;
     }
-    char operations[4] = { '+', '-', '*', '/' };
+    char operations[5] = { '+', '*', '-', '/', '\0' };
     Stack* stack = createStack();
     char* postfix = calloc(lengthOfInfix, sizeof(char));
     if (postfix == NULL)
@@ -25,15 +37,65 @@ char* convertToPostfixForm(char infix[], int* errorCode)
     int currentIndex = 0;
     for (int i = 0; i < lengthOfInfix; ++i)
     {
-        if ((int) infix[i] >= (int) '0' && (int) infix[i] <= (int) '9' ||
-            infix[i] == ' ')
+        int indexOfOperation = findElement(operations, infix[i]);
+        if ( infix[i] >= '0' && infix[i] <= '9' )
         {
-            postfix[currentIndex] = infix[i];
-            ++currentIndex;
+            postfix[currentIndex++] = infix[i];
+            postfix[currentIndex++] = ' ';
         }
-        else
+        else if (indexOfOperation != -1)
         {
+            while (!stackIsEmpty(&stack) && (stack->value) != '(' &&
+                    findElement(operations, stack->value) % 2 >= indexOfOperation % 2)
+            {
+                postfix[currentIndex++] = pop(&stack, errorCode);
+                postfix[currentIndex++] = ' ';
+                if (*errorCode != 0)
+                {
+                    free(postfix);
+                    deleteStack(&stack);
+                    return NULL;
+                }
+            }
+            push(&stack, infix[i]);
+        }
+        else if (infix[i] == '(')
+        {
+            push(&stack, '(');
+        }
+        else if (infix[i] == ')')
+        {
+            while ((stack->value) != '(')
+            {
+                postfix[currentIndex++] = pop(&stack, errorCode);
+                postfix[currentIndex++] = ' ';
+                if (*errorCode != 0)
+                {
+                    free(postfix);
+                    deleteStack(&stack);
+                    return NULL;
+                }
+            }
+            pop(&stack, errorCode);
+            if (*errorCode != 0)
+            {
+                free(postfix);
+                deleteStack(&stack);
+                return NULL;
+            }
+        }
+    }
+    while (!stackIsEmpty(&stack))
+    {
+        postfix[currentIndex++] = pop(&stack, errorCode);
+        postfix[currentIndex++] = ' ';
+        if (*errorCode != 0)
+        {
+            free(postfix);
+            deleteStack(&stack);
             return NULL;
         }
     }
+    deleteStack(&stack);
+    return postfix;
 }
