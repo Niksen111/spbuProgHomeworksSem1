c1d8246da45819307a4c1ca7aed232159be2a507
Niksen111 <nikiksen2003@gmail.com>
2021-11-08 22:08:00 +0300

    Created SearchTree project, added .c .h files, writed SearchTree structure, writed functions for SearchTree structure

---
+++ SeventhHomework26.10/SearchTree/searchTree.c
@@ -0,0 +1,214 @@
+#include "searchTree.h"
+#include <stdlib.h>
+
+typedef struct TreeNode
+{
+    int key;
+    char* value;
+    TreeNode* parent;
+    TreeNode* leftSon;
+    TreeNode* rightSon;
+} TreeNode;
+
+typedef struct TreeRoot
+{
+    TreeNode* treeRoot;
+} TreeRoot;
+
+typedef struct CurrentTreeNode
+{
+    TreeNode* currentTreeNode;
+} CurrentTreeNode;
+
+TreeRoot* createTreeRoot()
+{
+    return NULL;
+}
+
+void deleteBranch(CurrentTreeNode** branch)
+{
+    if ((*branch)->currentTreeNode->leftSon != NULL)
+    {
+        CurrentTreeNode* leftSon = calloc(1, sizeof(CurrentTreeNode));
+        leftSon->currentTreeNode = (*branch)->currentTreeNode->leftSon;
+        deleteBranch(&leftSon);
+    }
+    if ((*branch)->currentTreeNode->rightSon != NULL)
+    {
+        CurrentTreeNode* rightSon = calloc(1, sizeof(CurrentTreeNode));
+        rightSon->currentTreeNode = (*branch)->currentTreeNode->rightSon;
+        deleteBranch(&rightSon);
+    }
+    free((*branch)->currentTreeNode->value);
+    free((*branch)->currentTreeNode->leftSon);
+    free((*branch)->currentTreeNode->rightSon);
+    free((*branch)->currentTreeNode->parent);
+    free(*branch);
+}
+
+void deleteTree(TreeRoot** root)
+{
+    CurrentTreeNode* removablePart = calloc(1, sizeof(CurrentTreeNode));
+    removablePart->currentTreeNode = (*root)->treeRoot;
+    deleteBranch(&removablePart);
+    free(*root);
+}
+
+void addTreeNode(TreeRoot* root, int key, char* value)
+{
+
+}
+
+bool isLeftSon(CurrentTreeNode* currentTreeNode)
+{
+    return currentTreeNode->currentTreeNode->parent->leftSon
+        == currentTreeNode->currentTreeNode;
+}
+
+char* removeTreeNode(CurrentTreeNode** retrievableNode)
+{
+    char* currentValue = (*retrievableNode)->currentTreeNode->value;
+    if ((*retrievableNode)->currentTreeNode->leftSon == NULL
+        && (*retrievableNode)->currentTreeNode->rightSon == NULL)
+    {
+        deleteBranch(retrievableNode);
+        return currentValue;
+    }
+    if ((*retrievableNode)->currentTreeNode->leftSon != NULL
+        && (*retrievableNode)->currentTreeNode->rightSon == NULL)
+    {
+        if (isLeftSon(*retrievableNode))
+        {
+            (*retrievableNode)->currentTreeNode->parent->leftSon =
+                    (*retrievableNode)->currentTreeNode->leftSon;
+            (*retrievableNode)->currentTreeNode->leftSon->parent =
+                    (*retrievableNode)->currentTreeNode->parent;
+            (*retrievableNode)->currentTreeNode->leftSon = NULL;
+            deleteBranch(retrievableNode);
+
+            return currentValue;
+        }
+        (*retrievableNode)->currentTreeNode->parent->rightSon =
+                (*retrievableNode)->currentTreeNode->leftSon;
+        (*retrievableNode)->currentTreeNode->leftSon->parent =
+                (*retrievableNode)->currentTreeNode->parent;
+        (*retrievableNode)->currentTreeNode->leftSon = NULL;
+        deleteBranch(retrievableNode);
+
+        return currentValue;
+    }
+    if ((*retrievableNode)->currentTreeNode->leftSon == NULL
+        && (*retrievableNode)->currentTreeNode->rightSon != NULL)
+    {
+        if (isLeftSon(*retrievableNode))
+        {
+            (*retrievableNode)->currentTreeNode->parent->leftSon =
+                    (*retrievableNode)->currentTreeNode->rightSon;
+            (*retrievableNode)->currentTreeNode->rightSon->parent =
+                    (*retrievableNode)->currentTreeNode->parent;
+            (*retrievableNode)->currentTreeNode->rightSon = NULL;
+            deleteBranch(retrievableNode);
+
+            return currentValue;
+        }
+        (*retrievableNode)->currentTreeNode->parent->rightSon =
+                (*retrievableNode)->currentTreeNode->rightSon;
+        (*retrievableNode)->currentTreeNode->rightSon->parent =
+                (*retrievableNode)->currentTreeNode->parent;
+        (*retrievableNode)->currentTreeNode->rightSon = NULL;
+        deleteBranch(retrievableNode);
+
+        return currentValue;
+    }
+    CurrentTreeNode* newNode = calloc(1, sizeof(CurrentTreeNode));
+    newNode->currentTreeNode = (*retrievableNode)->currentTreeNode->leftSon;
+    while (newNode->currentTreeNode->rightSon != NULL)
+    {
+        newNode->currentTreeNode = newNode->currentTreeNode->rightSon;
+    }
+    (*retrievableNode)->currentTreeNode->key = newNode->currentTreeNode->key;
+    (*retrievableNode)->currentTreeNode->value = newNode->currentTreeNode->value;
+    if (newNode->currentTreeNode->leftSon != NULL)
+    {
+
+    }
+}
+
+char* findValue(TreeRoot* root, int key)
+{
+    CurrentTreeNode* node = calloc(1, sizeof(CurrentTreeNode));
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    node->currentTreeNode = root->treeRoot;
+    while (true)
+    {
+        if (key == node->currentTreeNode->key)
+        {
+            char* value = node->currentTreeNode->value;
+            free(node);
+            return value;
+        }
+        if (key > node->currentTreeNode->key)
+        {
+            if (node->currentTreeNode->rightSon == NULL)
+            {
+                free(node);
+                return NULL;
+            }
+            node->currentTreeNode = node->currentTreeNode->rightSon;
+        }
+        else
+        {
+            if (node->currentTreeNode->leftSon == NULL)
+            {
+                free(node);
+                return NULL;
+            }
+            node->currentTreeNode = node->currentTreeNode->leftSon;
+        }
+    }
+}
+
+bool isKeyInTree(TreeRoot* root, int key)
+{
+    CurrentTreeNode* node = calloc(1, sizeof(CurrentTreeNode));
+    if (node == NULL)
+    {
+        return false;
+    }
+    node->currentTreeNode = root->treeRoot;
+    while (true)
+    {
+        if (key == node->currentTreeNode->key)
+        {
+            free(node);
+            return true;
+        }
+        if (key > node->currentTreeNode->key)
+        {
+            if (node->currentTreeNode->rightSon == NULL)
+            {
+                free(node);
+                return false;
+            }
+            node->currentTreeNode = node->currentTreeNode->rightSon;
+        }
+        else
+        {
+            if (node->currentTreeNode->leftSon == NULL)
+            {
+                free(node);
+                return false;
+            }
+            node->currentTreeNode = node->currentTreeNode->leftSon;
+        }
+    }
+}
+
+// Deletes the given key and its associated value from the dictionary.
+void removeEntry(TreeRoot** root, int key)
+{
+
+}
---
+++ SeventhHomework26.10/SearchTree/searchTree.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <stdbool.h>
+
+// node of tree
+typedef struct TreeNode TreeNode;
+
+// root of tree
+typedef struct TreeRoot TreeRoot;
+
+// create empty TreeRoot
+TreeRoot* createTreeRoot();
+
+// removes the entire Tree
+void deleteTree(TreeRoot** root);
+
+// adds a new node to the tree or replaces the value
+// if a node with this key exists
+void addTreeNode(TreeRoot* root, int key, char* value);
+
+// finds a value for a given key in the dictionary
+char* findValue(TreeRoot* root, int key);
+
+// checks if the given key is in the dictionary
+bool isKeyInTree(TreeRoot* root, int key);
+
+// Deletes the given key and its associated value from the dictionary.
+void removeTheKey(TreeRoot** root, int key);
