0ddb1dabc1c30e5b9a89e42e6058d29f3b6fbdee
Niksen111 <nikiksen2003@gmail.com>
2021-12-06 21:32:16 +0300

    Writed HashTable structure

--- NinthHomework09.11.21/HashTable/hashTable.c
+++ NinthHomework09.11.21/HashTable/hashTable.c
@@ -1,2 +1,14 @@
 #include "hashTable.h"
 #include "list.h"
+
+typedef struct HashTable
+{
+    int array[100000];
+} HashTable;
+
+void addToHashTable(HashTable* hashTable, char* string)
+{
+    
+}
+
+void printHashTable(HashTable* hashTable);
